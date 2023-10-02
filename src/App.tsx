e505336a415ba11c7dfd323e0ca59c7283e7fe39
“Marco <marco.pianaroli96@gmail.com>
2023-10-02 19:56:10 +0200

    imports files from old project

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,73 @@
-export default function App() {
-  return <div>asd</div>;
+import "./App.css";
+import NavBar from "./components/NavBar";
+import BookCovers from "./components/BookCovers";
+import BookDescription from "./components/BookDescription";
+import { useState } from "react";
+import { Book } from "./types";
+
+import books from "../src/data/bookdata.json";
+
+function App() {
+  const bookList = books.library.map((item) => {
+    const {
+      title,
+      pages,
+      genre,
+      cover,
+      synopsis,
+      year,
+      ISBN,
+      author: { name, otherBooks },
+    } = item.book;
+
+    return {
+      title,
+      pages,
+      genre,
+      cover,
+      synopsis,
+      year,
+      ISBN,
+      author: {
+        name,
+        otherBooks,
+      },
+    };
+  });
+
+  const [selectedBook, setSelectedBook] = useState<Book | undefined>(undefined);
+
+  const selectedBookHandler = (book: Book) => {
+    setSelectedBook(book);
+  };
+
+  return (
+    <>
+      <div className="min-w-screen h-screen">
+        <header className="">
+          <NavBar />
+        </header>
+        <main className="flex ">
+          <section className="grid grid-cols-2 w-1/2  gap-4  ml-2 mt-3  sm:grid-cols-3 lg:grid-cols-4">
+            {bookList.map((book) => {
+              return (
+                <BookCovers
+                  cover={book.cover}
+                  alt={book.title}
+                  key={book.ISBN}
+                  info={book}
+                  onClick={selectedBookHandler}
+                />
+              );
+            })}
+          </section>
+          <section className="ml-3 w-1/2">
+            <BookDescription book={selectedBook} />
+          </section>
+        </main>
+      </div>
+    </>
+  );
 }
+
+export default App;
--- src/components/BookCovers.tsx
+++ src/components/BookCovers.tsx
@@ -1,3 +1,16 @@
-export default function BookCovers() {
-  return <div>asd</div>;
+export default function BooksCover(props) {
+  function selectedBookHandler(book) {
+    console.log(props.info);
+    props.onClick(props.info);
+  }
+  return (
+    <>
+      <div
+        className="border-2 border-black "
+        onClick={() => selectedBookHandler(props.book)}
+      >
+        <img className="w-full h-full" src={props.cover} alt={props.title} />
+      </div>
+    </>
+  );
 }
